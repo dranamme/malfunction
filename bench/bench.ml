93e6137c258ea27e3d99639f3398a7f610b0a073
Emma Nardino <emma.nardino@ens-lyon.fr>
2025-07-28 15:14:03 +0200

    qcheck program generator

--- bench/bench.ml
+++ bench/bench.ml
@@ -8,7 +8,7 @@ open QCheck2
 
 let n_domains = Domain.recommended_domain_count () (* Degree of parallelism *)
 let seed = 1312 (* Seed for random tree gen. *)
-let size = 1000000 (* Expr size *)
+let size = 100000 (* Expr size *)
 
 (* let () = *)
 (*   Fmt.pr "Configuration: %d domains, %d seed, %d tree size@." *)
@@ -41,24 +41,32 @@ let rec gen_int_expr size : t =
 
 let numop2 op l r = Mnumop2(op, `Int, l, r)
 
+let vecnew len def = Mvecnew(`Array, len, def)
+
 let switch e l = Mswitch (e, l)
 
 let num n = Mnum (`Int n)
 
 
+let tag n = `Tag n
 
 
-let gen_prog_qchek size =
+
+let gen_prog_qcheck size =
   size |> Gen.(fix (fun self n ->
                    match n with
-                   | 0 -> map num nat
+                   | 0 -> num <$> nat
                    | n ->
                       frequency
-                        [1, map num nat;
-                         2, map3 numop2
+                        [3, num <$> nat;
+                         5, map3 numop2
                               (oneofl [`Add; `Sub; `Mul; `Div; `Mod])
                               (self (n/2))
-                              (self (n/2))]
+                              (self (n/2));
+                         1, map2 vecnew (self (n/2)) (self (n/2));
+                         4, map2 switch (self (n/2))
+                              (list @@ pair (list (tag <$> nat)) (self (n/2)))
+                        ]
           ))
 
 
@@ -66,7 +74,7 @@ let gen_prog_qchek size =
 
 
 let _ =
-  to_lambda Env.empty (gen_int_expr size)
+  to_lambda_tmca Env.empty (Gen.generate1 (gen_prog_qcheck size))
 
 
 (* let gen_vector_op size : t = *)
