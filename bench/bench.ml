243ba640d09f9f49ef22a749ea1b7ef494da9443
Gabriel Radanne <gabriel.radanne@inria.fr>
2025-07-29 14:03:05 +0200

    More iterations.

--- bench/bench.ml
+++ bench/bench.ml
@@ -6,38 +6,12 @@ open Malfunction_compiler
 open QCheck2
 
 
-let n_domains = Domain.recommended_domain_count () (* Degree of parallelism *)
+let n_domains = 4 (* Degree of parallelism *)
 let seed = 1312 (* Seed for random tree gen. *)
-let size = 100000 (* Expr size *)
 
-(* let () = *)
-(*   Fmt.pr "Configuration: %d domains, %d seed, %d tree size@." *)
-(*     n_domains seed size *)
-
-let () = Random.init seed
-
-
-let gen_vector_type () =
-  match Random.int 2 with
-  | 0 -> `Array
-  | _ -> `Bytevec
-
-let rec gen_int_expr size : t =
-  if size = 0 then
-    Mnum (`Int (Random.int_in_range ~min:(-42000) ~max:42000))
-  else
-    let op = match Random.int 5 with
-      | 0 -> `Add
-      | 1 -> `Sub
-      | 2 -> `Mul
-      | 3 -> `Div
-      | _ -> `Mod
-    in
-    let left_size = Random.int size in
-    Mnumop2 (op, `Int, gen_int_expr left_size, gen_int_expr (size - left_size - 1))
-
-
-
+let () =
+  Fmt.pr "Configuration: %d domains, %d seed@."
+    n_domains seed
 
 let numop2 op l r = Mnumop2(op, `Int, l, r)
 
@@ -67,28 +41,27 @@ let gen_prog_qcheck =
     num <$> nat
   else
     frequency
-      [1, num <$> nat;
-       1, veclen <$> (self (n-1));
-       3, n-1 |> gen_sum >>= (fun (k, nk) ->
+      [1, veclen <$> (self (n-1));
+       4, n-1 |> gen_sum >>= (fun (k, nk) ->
            map3 numop2
              (oneofl [`Add; `Sub; `Mul; `Div; `Mod])
              (self k)
              (self nk));
-       1, n-1 |> gen_sum >>= (fun (k, nk) ->
+       2, n-1 |> gen_sum >>= (fun (k, nk) ->
            map2 vecnew (self k) (self nk));
-       4, (let* k, nk = gen_sum ~bound:10 (n-1) in
+       20, (let* k, nk = gen_sum ~bound:10 (n-1) in
            let* t = self k in
            let* t' = self nk in
            pure @@ Mlet ([`Unnamed t], t')
           );
        2, n-1 |> gen_sum >>= (fun (k, nk) ->
            map2 switch (self k)
-             ((1 -- 1312) >>= (fun k -> list_size (pure k) @@ pair (list_size (1--7)
-                                                                      (tag <$> nat)) (self (nk/k)))))
+             ((1 -- 10) >>= fun len ->
+              list_size (pure len) @@
+              pair
+                (list_size (pure 1) (tag <$> nat))
+                (self (nk/len))))
       ]
-
-
-
 
 let rec size_of_term t =
   match t with
@@ -110,31 +83,23 @@ and size_of_binding_list l =
                      List.fold_left (fun i (_,t) -> i + size_of_term t) i l) 0 l
 
 
-
-
-
-
-(* let _ = *)
-(*   to_lambda_tmca Env.empty (Gen.generate1 (gen_prog_qcheck size)) *)
-
-
-(* let gen_vector_op size : t = *)
-(*   match Random.int with *)
-(*   | *)
-
-
-(* let gen_prog () : t = *)
-(*   Mnum (`Int 3) *)
-
 let rand = Random.State.make [| seed |]
 
-(* range from 10¹ to 10⁴ elements, 5 points per order. *)
-let args = List.init 16 (fun x -> int_of_float (10. ** (float x/.5. +. 1.)))
+(* range from 10¹ to 10⁶ elements, 5 points per order. *)
+let args = List.init 26 (fun x -> int_of_float (10. ** (float x/.5. +. 1.)))
 let trees =
   List.map (fun i -> i, Gen.generate1 ~rand @@ gen_prog_qcheck i) args
   |> List.to_seq
   |> Hashtbl.of_seq
 
+let () =    
+  Format.printf "@[<v2>Sizes : {@ ";
+  Seq.iter
+    (fun (i, t) -> Format.printf "%i: %i;@," i (size_of_term t))
+    (Hashtbl.to_seq trees);
+  Format.printf "}@]";
+  ()
+  
 open Picos
 open Bechamel
 
